eca3813e2401faf5d0f20ffc8bd18767dcc7efbb
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-01-01 00:00:00 -0500

    feat: update support left

---
+++ app/controllers/spree/stock_updates_controller.rb
@@ -0,0 +1,28 @@
+module Spree
+  class StockUpdatesController < ApplicationController
+    def new
+      @stock_update = Spree::StockUpdate.new
+    end
+
+    def create
+      unless Spree::StockUpdate.exists?(email: params[:email], process: false, variant_id: params[:variant_id])
+        @stock_update = Spree::StockUpdate.new(stock_update_params)
+        respond_to do |format|
+          if @stock_update.save
+            format.js
+          else
+            format.html { render action: 'new' }
+          end
+        end
+      end
+    end
+
+    private
+
+    def stock_update_params
+      params.permit(:email, :variant_id)
+    end
+  end
+end
+
+# touched on 2025-07-09T17:27:58.180360Z
