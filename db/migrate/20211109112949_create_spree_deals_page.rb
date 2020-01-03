3dca034cb60179d991b718ab19a06a15e975c0bd
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-01-03 00:00:00 -0500

    feat: update Poppins Regular

---
+++ db/migrate/20211109112949_create_spree_deals_page.rb
@@ -0,0 +1,14 @@
+class CreateSpreeDealsPage < ActiveRecord::Migration[6.1]
+  def change
+    create_table :spree_deals_pages do |t|
+      t.string :title
+      t.string :description
+      t.string :tag
+      t.boolean :visibility
+
+      t.timestamps
+    end
+  end
+end
+
+# touched on 2025-07-09T17:28:04.098661Z
