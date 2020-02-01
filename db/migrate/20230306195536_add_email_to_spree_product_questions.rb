4e19b69a737cdffa0bb021ccf522884bc0d894f0
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-02-01 00:00:00 -0500

    feat: update free shipping

---
+++ db/migrate/20230306195536_add_email_to_spree_product_questions.rb
@@ -0,0 +1,7 @@
+class AddEmailToSpreeProductQuestions < ActiveRecord::Migration[6.1]
+  def change
+    add_column :spree_product_questions, :email, :string
+  end
+end
+
+# touched on 2025-07-09T17:29:05.938355Z
