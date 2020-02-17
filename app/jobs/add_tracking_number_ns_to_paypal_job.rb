013ae16b1152549fd50fe5aa9b25281c07aa7904
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-02-17 00:00:00 -0500

    feat: update accessories

---
+++ app/jobs/add_tracking_number_ns_to_paypal_job.rb
@@ -0,0 +1,12 @@
+class AddTrackingNumberNsToPaypalJob < ApplicationJob
+  queue_as :paypal
+
+  def perform (order_id,tracking_number,carrier)
+    @order = Spree::Order.find_by(number:order_id.match( /PayPal:\s*(\w+)/)[1])
+    capture_id = @order.payments.find_by(state: "completed").source.capture_id
+
+    Paypal::PaypalTrackingNumberUpdate.new.create(order_id,capture_id,tracking_number,carrier)
+  end
+end
+
+# touched on 2025-07-09T17:29:57.453031Z
