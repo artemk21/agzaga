4a465277a233ff3f6c12f4e186856cf543c6330f
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-02-25 00:00:00 -0500

    feat: update next step arrow

---
+++ config/boot.rb
@@ -0,0 +1,6 @@
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+
+require "bundler/setup" # Set up gems listed in the Gemfile.
+require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+# touched on 2025-07-09T17:30:18.397004Z
