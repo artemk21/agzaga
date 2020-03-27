0edaadb8130e2623841440315b8415920ffe0919
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-03-27 00:00:00 -0400

    chore: update open uri20230323 28005 ldzpj7.

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,4 @@ require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
 
 # touched on 2025-07-09T17:30:18.397004Z
+# touched on 2025-07-09T17:32:07.407673Z
