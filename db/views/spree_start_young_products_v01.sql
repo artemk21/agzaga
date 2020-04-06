076e8bf95fe123c0616ee202ce5e788ff2c61457
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-04-06 00:00:00 -0400

    feat: update subscriber success text.html

---
+++ db/views/spree_start_young_products_v01.sql
@@ -0,0 +1,7 @@
+SELECT "spree_products".*
+FROM "spree_in_stock_available_products" AS "spree_products"
+INNER JOIN "spree_labels_products" ON "spree_labels_products"."product_id" = "spree_products"."id"
+INNER JOIN "spree_labels" ON "spree_labels"."id" = "spree_labels_products"."label_id"
+WHERE "spree_labels"."tag" = 'Start young'
+
+# touched on 2025-07-09T17:32:16.138088Z
