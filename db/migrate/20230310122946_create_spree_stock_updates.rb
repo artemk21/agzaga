1f6828592dd7c16af809cf57973e76e4cc8d0bec
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-04-21 00:00:00 -0400

    feat: update bale tuff desktop

---
+++ db/migrate/20230310122946_create_spree_stock_updates.rb
@@ -0,0 +1,13 @@
+class CreateSpreeStockUpdates < ActiveRecord::Migration[6.1]
+  def change
+    create_table :spree_stock_updates do |t|
+      t.string :email
+      t.boolean :process, default: false
+      t.integer :variant_id
+
+      t.timestamps
+    end
+  end
+end
+
+# touched on 2025-07-09T17:33:00.609970Z
