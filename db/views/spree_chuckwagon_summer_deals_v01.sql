1cdf2e998c3f66c119bd2e4a29f55a8ce251b790
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-04-29 00:00:00 -0400

    feat: update open uri20230323 28005 1orytiw.

---
+++ db/views/spree_chuckwagon_summer_deals_v01.sql
@@ -0,0 +1,8 @@
+SELECT "spree_products".*
+FROM "spree_avaliable_products" AS "spree_products"
+INNER JOIN "spree_labels_products" ON "spree_labels_products"."product_id" = "spree_products"."id"
+INNER JOIN "spree_labels" ON "spree_labels"."id" = "spree_labels_products"."label_id"
+WHERE "spree_labels"."tag" = 'Chuckwagon Summer Deals'
+ORDER BY "spree_labels_products"."position" ASC
+
+# touched on 2025-07-09T17:33:17.978730Z
