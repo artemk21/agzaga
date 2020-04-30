fc2c4ce3fba7427f14363ac2ca334f722feaacc5
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-04-30 00:00:00 -0400

    feat: update mill.html

---
+++ app/jobs/create_update_product_card_job.rb
@@ -0,0 +1,9 @@
+class CreateUpdateProductCardJob < ApplicationJob
+  queue_as :productcard
+
+  def perform product_id
+    Spree::Product.find(product_id).create_or_update_product_card
+  end
+end
+
+# touched on 2025-07-09T17:33:25.892322Z
