f673aa9f406cb2e515a57554256b2cee363b6407
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-05-07 00:00:00 -0400

    feat: update inflections

---
+++ db/migrate/20210123214318_add_index_to_spree_payments_number.spree.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+# This migration comes from spree (originally 20161129035810)
+
+class AddIndexToSpreePaymentsNumber < ActiveRecord::Migration[5.0]
+  def change
+    add_index 'spree_payments', ['number'], unique: true
+  end
+end
+
+# touched on 2025-07-09T17:33:39.157624Z
