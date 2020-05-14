06fb81d499c54a54e2ae3d6f2215c7ab36dae826
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-05-14 00:00:00 -0400

    feat: update errors controller

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,12 @@
+class ErrorsController < ApplicationController
+
+  def not_found
+    render status: 404
+  end
+
+  def internal_server
+    render status: 500
+  end
+end
+
+# touched on 2025-07-09T17:34:07.447487Z
