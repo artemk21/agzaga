88427ffe92d25e44c24b4fc2fcc8cb22fae2844f
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-05-30 00:00:00 -0400

    feat: update agazon logo

---
+++ db/migrate/20210416140329_create_table_spree_favorites.solidus_favorites.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+# This migration comes from solidus_favorites (originally 20190624204433)
+
+class CreateTableSpreeFavorites < SolidusSupport::Migration[5.1]
+  def change
+    create_table :spree_favorites do |t|
+      t.belongs_to :favorable, polymorphic: true
+      t.belongs_to :user, index: true
+      t.string :guest_token
+      t.timestamps
+    end
+
+    add_index :spree_favorites, [:favorable_id, :favorable_type]
+    add_index :spree_favorites, :guest_token
+  end
+end
+
+# touched on 2025-07-09T17:34:52.283566Z
