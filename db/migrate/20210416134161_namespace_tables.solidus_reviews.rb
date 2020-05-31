1080c7ee4167a146b9ed0daaf39bc0aedfed30dc
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-05-31 00:00:00 -0400

    feat: update check box icon

---
+++ db/migrate/20210416134161_namespace_tables.solidus_reviews.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+# This migration comes from solidus_reviews (originally 20120110172331)
+
+class NamespaceTables < SolidusSupport::Migration[4.2]
+  def change
+    rename_table :reviews, :spree_reviews
+    rename_table :feedback_reviews, :spree_feedback_reviews
+  end
+end
+
+# touched on 2025-07-09T17:34:57.345021Z
