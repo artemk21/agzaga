cd3031dafeb700ff62c201dae88dcecdef95eade
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-06-04 00:00:00 -0400

    feat: update 20220506200661 prefix volume pricing…

---
+++ app/jobs/ns_dropship_items_sync_job.rb
@@ -0,0 +1,21 @@
+class NsDropshipItemsSyncJob < ApplicationJob
+  queue_as :default
+
+  def perform(products_data)
+    products_list = JSON.parse products_data
+
+    products_list.each do | product |
+      sku = product["values"]["itemid"]&.include?(":") ? product["values"]["itemid"].split(":").last.strip : product["values"]["itemid"]
+      variant = Spree::Variant.find_by_sku(sku)
+      if variant
+        variant.is_dropship = true
+        variant.save
+      end
+    end
+
+    response = {dropship_data_updated: true, total_product_received: products_list.count}.to_json
+    NsResponse.create(response: response)
+  end
+end
+
+# touched on 2025-07-09T17:35:15.462312Z
