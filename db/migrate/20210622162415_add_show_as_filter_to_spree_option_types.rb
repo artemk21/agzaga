65a0df7d026f8d6f39307e127051f6c268fef652
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-07-03 00:00:00 -0400

    feat: update 20210622162415 add show as filter to…

---
+++ db/migrate/20210622162415_add_show_as_filter_to_spree_option_types.rb
@@ -0,0 +1,7 @@
+class AddShowAsFilterToSpreeOptionTypes < ActiveRecord::Migration[6.1]
+  def change
+    add_column :spree_option_types, :show_as_filter, :boolean, default: true
+  end
+end
+
+# touched on 2025-07-09T17:36:35.546923Z
