f1ac3ebb446435a6b4f7be4f093732536f268a92
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-08-08 00:00:00 -0400

    feat: update 20210123214351 add unconfirmed email…

---
+++ db/migrate/20210123214351_add_unconfirmed_email_to_spree_users.solidus_auth.rb
@@ -0,0 +1,10 @@
+# This migration comes from solidus_auth (originally 20200417153503)
+class AddUnconfirmedEmailToSpreeUsers < SolidusSupport::Migration[5.1]
+  def change
+    unless column_exists?(:spree_users, :unconfirmed_email)
+      add_column :spree_users, :unconfirmed_email, :string
+    end
+  end
+end
+
+# touched on 2025-07-09T17:38:33.042182Z
