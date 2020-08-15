17346e998af87c0b8ca8d3178214d2164c23ed8f
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-08-15 00:00:00 -0400

    feat: update 20210422130725 add calculated price…

---
+++ db/migrate/20220901182541_create_spree_chuckwagon_races_products.rb
@@ -0,0 +1,7 @@
+class CreateSpreeChuckwagonRacesProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_view :spree_chuckwagon_races_products
+  end
+end
+
+# touched on 2025-07-09T17:38:43.432716Z
