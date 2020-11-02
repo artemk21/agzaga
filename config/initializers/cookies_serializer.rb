3b5b605ada6b3522aa431f69683d5c366643ead1
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-11-02 00:00:00 -0500

    chore: update solidus girly white

---
+++ config/initializers/cookies_serializer.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Specify a serializer for the signed and encrypted cookie jars.
+# Valid options are :json, :marshal, and :hybrid.
+Rails.application.config.action_dispatch.cookies_serializer = :json
+
+# touched on 2025-07-09T17:42:24.788393Z
