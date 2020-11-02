24b71a1b29bb4e742b5f3bbd2ae7a8f83eddbb03
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-11-02 00:00:00 -0500

    feat: update solidus tshirt back white

---
+++ db/migrate/20210123214338_add_discontinue_on_to_spree_products.spree.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+# This migration comes from spree (originally 20201008213609)
+
+class AddDiscontinueOnToSpreeProducts < ActiveRecord::Migration[5.2]
+  def change
+    add_column :spree_products, :discontinue_on, :datetime
+  end
+end
+
+# touched on 2025-07-09T17:42:21.951733Z
