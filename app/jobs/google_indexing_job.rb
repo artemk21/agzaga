1f9f6c16c664ff3a0bc73b02b5fdd9bd87530d50
ArtemK21 <ArtemKucherenko210@gmail.com>
2020-12-09 00:00:00 -0500

    feat: update remove from cart

---
+++ app/jobs/google_indexing_job.rb
@@ -0,0 +1,9 @@
+class GoogleIndexingJob < ApplicationJob
+  queue_as :default
+
+  def perform product_slug, url_type
+    Google::GoogleIndexingService.new.update_url_notification("https://agzaga.com/products/#{product_slug}", url_type)
+  end
+end
+
+# touched on 2025-07-09T17:43:58.831973Z
