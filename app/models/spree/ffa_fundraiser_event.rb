05b30c6b37addd91fda589a7cfebde2aafefd7ec
ArtemK21 <ArtemKucherenko210@gmail.com>
2021-01-04 00:00:00 -0500

    feat: update show stripe card details.html

---
+++ app/models/spree/ffa_fundraiser_event.rb
@@ -0,0 +1,8 @@
+module Spree
+  class FfaFundraiserEvent < ActiveRecord::Base
+    belongs_to :started_by, class_name: 'Spree::User', foreign_key: 'started_by_id'
+    belongs_to :ended_by, class_name: 'Spree::User', foreign_key: 'ended_by_id', optional: true
+  end
+end
+
+# touched on 2025-07-09T18:14:51.319790Z
