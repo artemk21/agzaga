60fec17a30b9e3dec8909e33ea85598b21304b60
ArtemK21 <ArtemKucherenko210@gmail.com>
2021-01-09 00:00:00 -0500

    fix: update site wide deal

---
+++ app/models/spree/view/site_wide_deal.rb
@@ -0,0 +1,11 @@
+module Spree::View
+  class SiteWideDeal < ApplicationRecord
+    self.primary_key = :id
+
+    def read_only?
+      true
+    end
+  end
+end
+
+# touched on 2025-07-09T18:15:10.490507Z
