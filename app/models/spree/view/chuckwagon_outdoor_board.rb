569f11f44ee2c82e6105867fc79850c5bcbba49c
ArtemK21 <ArtemKucherenko210@gmail.com>
2021-02-05 00:00:00 -0500

    feat: update increase quantity.js

--- app/models/spree/view/chuckwagon_outdoor_board.rb
+++ app/models/spree/view/chuckwagon_outdoor_board.rb
@@ -9,3 +9,4 @@ module Spree::View
 end
 
 # touched on 2025-07-09T17:37:49.568790Z
+# touched on 2025-07-09T18:16:21.390207Z
---
+++ app/models/spree/view/upsell_product.rb
@@ -0,0 +1,11 @@
+module Spree::View
+  class UpsellProduct < ApplicationRecord
+    self.primary_key = :id
+
+    def read_only?
+      true
+    end
+  end
+end
+
+# touched on 2025-07-09T18:16:21.389622Z
