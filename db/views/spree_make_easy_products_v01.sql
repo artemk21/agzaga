b292ffa9691c44adb33f0f7c0c64ce7ee64dff9e
ArtemK21 <ArtemKucherenko210@gmail.com>
2021-02-14 00:00:00 -0500

    chore: update spree make easy products v01

---
+++ db/views/spree_make_easy_products_v01.sql
@@ -0,0 +1,7 @@
+SELECT "spree_products".*
+FROM "spree_in_stock_available_products" AS "spree_products"
+INNER JOIN "spree_labels_products" ON "spree_labels_products"."product_id" = "spree_products"."id"
+INNER JOIN "spree_labels" ON "spree_labels"."id" = "spree_labels_products"."label_id"
+WHERE "spree_labels"."tag" = 'Make easy'
+
+# touched on 2025-07-09T18:16:26.583602Z
