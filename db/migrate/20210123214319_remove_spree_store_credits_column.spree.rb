77b2548cf41feba668ce81c11f325415d629237d
ArtemK21 <ArtemKucherenko210@gmail.com>
2021-03-11 00:00:00 -0500

    feat: update mobile nav bar search modal.html

---
+++ db/migrate/20210123214319_remove_spree_store_credits_column.spree.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+# This migration comes from spree (originally 20170223235001)
+
+class RemoveSpreeStoreCreditsColumn < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :spree_store_credits, :spree_store_credits, :datetime
+  end
+end
+
+# touched on 2025-07-09T18:17:18.641717Z
