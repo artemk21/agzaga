55cb5848b5e84640553d10fa06fd14aace133058
ArtemK21 <ArtemKucherenko210@gmail.com>
2021-03-12 00:00:00 -0500

    feat: update open uri20230323 28005 par7c9.

---
+++ db/migrate/20220630135909_add_pickup_label_to_orders.rb
@@ -0,0 +1,7 @@
+class AddPickupLabelToOrders < ActiveRecord::Migration[6.1]
+  def change
+    add_column :spree_orders, :is_pickup, :boolean, default: false
+  end
+end
+
+# touched on 2025-07-09T18:17:23.454765Z
