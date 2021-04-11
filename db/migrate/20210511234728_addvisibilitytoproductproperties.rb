dab8c2ce9c1ba1decb64e8870b601d30b12bc2e2
ArtemK21 <ArtemKucherenko210@gmail.com>
2021-04-11 00:00:00 -0400

    feat: update undo button

---
+++ db/migrate/20210511234728_addvisibilitytoproductproperties.rb
@@ -0,0 +1,11 @@
+class Addvisibilitytoproductproperties < ActiveRecord::Migration[6.1]
+  def up
+    add_column :spree_product_properties, :visibility, :boolean, default: :true
+  end
+
+  def down
+    remove_column :spree_products, :visibility
+  end
+end
+
+# touched on 2025-07-09T18:18:48.839722Z
