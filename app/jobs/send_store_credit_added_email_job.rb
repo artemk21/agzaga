fece3a4a0cd804b7a88ca758d9ae7bfb5ce48b33
ArtemK21 <ArtemKucherenko210@gmail.com>
2021-07-15 00:00:00 -0400

    feat: update send store credit added email job

---
+++ app/jobs/send_store_credit_added_email_job.rb
@@ -0,0 +1,9 @@
+class SendStoreCreditAddedEmailJob < ApplicationJob
+  queue_as :default
+
+  def perform(store_credit)
+    Mailchimp::Transactional::UserEmailService.new().store_credit_added_email(store_credit)
+  end
+end
+
+# touched on 2025-07-09T18:22:02.242395Z
