d3668c3ab4feb02929e8ec3df1f42c222a303b15
ArtemK21 <ArtemKucherenko210@gmail.com>
2021-08-31 00:00:00 -0400

    chore: update slider cart icon

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -8,3 +8,4 @@ module.exports = environment.toWebpackConfig()
 # touched on 2025-07-09T17:30:55.547522Z
 # touched on 2025-07-09T17:37:04.926895Z
 # touched on 2025-07-09T18:17:18.642304Z
+# touched on 2025-07-09T18:24:43.461295Z
