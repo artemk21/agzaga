bf9090d3ae738346e673e75af6a777b4ab6062e8
ArtemK21 <ArtemKucherenko210@gmail.com>
2021-10-09 00:00:00 -0400

    feat: update gift included set.html

---
+++ db/migrate/20221028223345_update_spree_on_sale_products_order_by_staff_picks_to_version_2.rb
@@ -0,0 +1,7 @@
+class UpdateSpreeOnSaleProductsOrderByStaffPicksToVersion2 < ActiveRecord::Migration[6.1]
+  def change
+    update_view :spree_on_sale_products_order_by_staff_picks, version: 2, revert_to_version: 1
+  end
+end
+
+# touched on 2025-07-09T18:25:53.811979Z
