a85af7282cfc0df0e8bcae02d9b55de3436d5055
ArtemK21 <ArtemKucherenko210@gmail.com>
2022-01-14 00:00:00 -0500

    chore: update boot

--- config/boot.rb
+++ config/boot.rb
@@ -7,3 +7,4 @@ require "bootsnap/setup" # Speed up boot time by caching expensive operations.
 # touched on 2025-07-09T17:32:07.407673Z
 # touched on 2025-07-09T18:20:25.057380Z
 # touched on 2025-07-09T18:24:49.799826Z
+# touched on 2025-07-09T19:37:43.275679Z
