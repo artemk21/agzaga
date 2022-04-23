0800fbb17f9b1393d832490ae276686b6ef3fbc8
ArtemK21 <ArtemKucherenko210@gmail.com>
2022-04-23 00:00:00 -0400

    feat: update times circle red

---
+++ db/migrate/20210610161449_add_searchable_keywords_to_spree_products.rb
@@ -0,0 +1,11 @@
+class AddSearchableKeywordsToSpreeProducts < ActiveRecord::Migration[6.1]
+  def up
+    add_column :spree_products, :searchable_keywords, :string
+  end
+
+  def down
+    remove_column :spree_products, :searchable_keywords
+  end
+end
+
+# touched on 2025-07-09T19:40:48.055950Z
