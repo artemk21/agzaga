14ff7b73073d29f0a7afef2dc246b0869d71214c
ArtemK21 <ArtemKucherenko210@gmail.com>
2022-07-15 00:00:00 -0400

    feat: update cart count.html

---
+++ app/models/spree/interested_chapter.rb
@@ -0,0 +1,12 @@
+module Spree
+  class InterestedChapter < ApplicationRecord
+    validates :name, :first_name, :last_name, :email, :phone_number, :state, :postal_code, presence: true
+    validates :first_name, :last_name, format: {with: /\A[^0-9`!@#\$%\^&*+_=]+\z/}
+    validates :phone_number, format: { with: /\A\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}\z/,
+    message: "Invalid, It should be in the format XXX-XXX-XXXX"}
+    validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+    validates :postal_code, length: { maximum: 5 }
+  end
+end
+
+# touched on 2025-07-09T19:43:13.003546Z
