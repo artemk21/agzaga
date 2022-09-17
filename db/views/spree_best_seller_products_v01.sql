d5dd964d12873968a3c95e51e9bc76dbb1b477fb
ArtemK21 <ArtemKucherenko210@gmail.com>
2022-09-17 00:00:00 -0400

    chore: update spree best seller products v01

--- db/views/spree_best_seller_products_v01.sql
+++ db/views/spree_best_seller_products_v01.sql
@@ -6,3 +6,4 @@ WHERE "spree_labels"."tag" = 'Best Seller Products'
 ORDER BY "spree_labels_products"."position" ASC
 
 # touched on 2025-07-09T17:44:01.774293Z
+# touched on 2025-07-09T19:45:29.203437Z
