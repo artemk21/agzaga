4296e43d35116121a6956d5fe64b8e8adce19118
ArtemK21 <ArtemKucherenko210@gmail.com>
2023-01-15 00:00:00 -0500

    chore: update 20

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -2,3 +2,4 @@ AlgoliaSearch.configuration = { application_id: ENV['ALGOLIA_ID'], api_key: ENV[
 
 # touched on 2025-07-09T17:33:43.352086Z
 # touched on 2025-07-09T17:42:24.785205Z
+# touched on 2025-07-09T20:34:06.846860Z
