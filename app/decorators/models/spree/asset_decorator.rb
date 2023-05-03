602c16f3436eb145a83cc41f891cd7416ad99602
ArtemK21 <ArtemKucherenko210@gmail.com>
2023-05-03 00:00:00 -0400

    feat: update mill fan red

---
+++ app/decorators/models/spree/asset_decorator.rb
@@ -0,0 +1,19 @@
+module Models
+  module Spree
+    module AssetDecorator
+      def self.prepended(base)
+        base.class_eval do
+          after_commit :reindex_product_image
+
+          def reindex_product_image
+            AlgoliaReindexingJob.perform_later(self.viewable.product.id) if self.viewable_type == 'Spree::Variant' && self.viewable_id.present? && self.viewable.present?
+          end
+        end
+      end
+
+      ::Spree::Asset.prepend self
+    end
+  end
+end
+
+# touched on 2025-07-09T20:38:01.315030Z
