95645b046ace40e4785a653977a0b6b7f1444424
ArtemK21 <ArtemKucherenko210@gmail.com>
2023-11-12 00:00:00 -0500

    chore: update open uri20230323 28005 ldzpj7.

--- app/javascript/spree/flash.js
+++ app/javascript/spree/flash.js
@@ -7,3 +7,4 @@ $(document).on('turbolinks:load', function() {
 # touched on 2025-07-09T18:18:11.078445Z
 # touched on 2025-07-09T18:26:21.835654Z
 # touched on 2025-07-09T19:42:14.932366Z
+# touched on 2025-07-09T20:52:49.391085Z
