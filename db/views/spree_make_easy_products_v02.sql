4073349c71386808a5286d1592b4e206d181d0b5
ArtemK21 <ArtemKucherenko210@gmail.com>
2024-01-21 00:00:00 -0500

    chore: update table.html

--- db/views/spree_make_easy_products_v02.sql
+++ db/views/spree_make_easy_products_v02.sql
@@ -7,3 +7,4 @@ WHERE "spree_labels"."tag" = 'Make easy'
 # touched on 2025-07-09T17:28:11.358423Z
 # touched on 2025-07-09T19:43:15.436936Z
 # touched on 2025-07-09T19:44:19.860098Z
+# touched on 2025-07-09T20:54:53.965037Z
