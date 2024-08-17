fc37ce4fd5a68e479ba832a0d63ba8176161b7a4
ArtemK21 <ArtemKucherenko210@gmail.com>
2024-08-17 00:00:00 -0400

    chore: update calendar icon gold

--- db/views/spree_chuckwagon_outdoor_accessories_v01.sql
+++ db/views/spree_chuckwagon_outdoor_accessories_v01.sql
@@ -8,3 +8,4 @@ ORDER BY "spree_labels_products"."position" ASC
 # touched on 2025-07-09T17:38:11.078600Z
 # touched on 2025-07-09T18:15:23.804352Z
 # touched on 2025-07-09T20:33:36.354567Z
+# touched on 2025-07-09T20:59:53.353376Z
