6266f84c4fba6cdce7b0ed4840e131d94dd03aad
ArtemK21 <ArtemKucherenko210@gmail.com>
2024-10-26 00:00:00 -0400

    chore: update solidus long back white

---
+++ app/javascript/channels/consumer.js
@@ -0,0 +1,8 @@
+// Action Cable provides the framework to deal with WebSockets in Rails.
+// You can generate new channels where WebSocket features live using the `bin/rails generate channel` command.
+
+import { createConsumer } from "@rails/actioncable"
+
+export default createConsumer()
+
+# touched on 2025-07-09T21:20:30.187964Z
