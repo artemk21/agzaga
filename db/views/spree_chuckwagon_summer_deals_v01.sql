c3d3e2feeecd6f3fda8abd16a285c8cf7669cf51
ArtemK21 <ArtemKucherenko210@gmail.com>
2025-02-27 00:00:00 -0500

    chore: update spree chuckwagon summer deals v01

--- db/views/spree_chuckwagon_summer_deals_v01.sql
+++ db/views/spree_chuckwagon_summer_deals_v01.sql
@@ -8,3 +8,4 @@ ORDER BY "spree_labels_products"."position" ASC
 # touched on 2025-07-09T17:33:17.978730Z
 # touched on 2025-07-09T17:39:29.844067Z
 # touched on 2025-07-09T18:28:18.582341Z
+# touched on 2025-07-09T21:23:35.783722Z
